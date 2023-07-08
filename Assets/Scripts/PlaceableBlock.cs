37e5e29a8d2bd3956e57567a39679cd76eb782c3
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-07-08 02:55:30 +0200

    new road

--- Assets/Scripts/PlaceableBlock.cs
+++ Assets/Scripts/PlaceableBlock.cs
@@ -9,13 +9,11 @@ public class PlaceableBlock : MonoBehaviour
     public bool isSelected = false;
     public bool selectable = true;
     private BlockPlacer placer;
-    private MeshRenderer meshRenderer;
     
 
     void Start()
     {
         placer = GameObject.Find("Placer").GetComponent<BlockPlacer>();
-        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     void OnMouseDown()
@@ -34,13 +32,10 @@ public class PlaceableBlock : MonoBehaviour
     public void Select()
     {
         isSelected = true;
-        if (!meshRenderer) meshRenderer = GetComponent<MeshRenderer>();
-        meshRenderer.material.color = Color.red;
     }
     public void Unselect()
     {
         isSelected = false;
-        meshRenderer.material.color = Color.white;
     }
 
     internal Vector3 getPos()
