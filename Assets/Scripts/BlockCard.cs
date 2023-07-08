5b6688e46766fa27fbfaace7cc8bef94fcf8505d
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-07-08 09:26:43 +0200

    show images

--- Assets/Scripts/BlockCard.cs
+++ Assets/Scripts/BlockCard.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockCard : MonoBehaviour
 {
     public BlockData data;
     BlockPlacer placer;
+    public Image image;
 
     private void Start()
     {
         placer = GameObject.Find("Placer").GetComponent<BlockPlacer>();
+        image.sprite = data.img;
     }
 
 
