61b3ea07cc7e127f8770f0754c8b6347178084ab
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-07-08 10:46:12 +0200

    ok

--- Assets/Scripts/ActionsUI.cs
+++ Assets/Scripts/ActionsUI.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ActionsUI : MonoBehaviour
@@ -10,8 +12,51 @@ public class ActionsUI : MonoBehaviour
         public ActionsTypes type;
         public GameObject prefab;
     }
-    public List<Action> prefabs;
 
+    public List<Action> prefabs;
+    public float timeAction = 3;
+    private float progressAction = 0;
+
+    private void Start()
+    {
+        progressAction = timeAction;
+    }
+
+    private void Update()
+    {
+        if (GameManager.Instance.gameState != GameState.RUN) return;
+        print("sdasa");
+        if (progressAction <= 0)
+        {
+            progressAction = timeAction;
+            if (GameManager.Instance.actionList.Count != 0)
+            {
+                var action = GameManager.Instance.actionList.FirstOrDefault();
+                switch (action)
+                {
+                    case ActionsTypes.UP:
+                        GameManager.Instance.car.ActionUp();
+                        break;
+                    case ActionsTypes.DOWN:
+                        GameManager.Instance.car.ActionDown();
+                        break;
+                    case ActionsTypes.LEFT:
+                        GameManager.Instance.car.ActionLeft();
+                        break;
+                    case ActionsTypes.RIGHT:
+                        GameManager.Instance.car.ActionRight();
+                        break;
+                }
+
+                GameManager.Instance.actionList.Remove(action);
+                //TODO
+                // while (transform.childCount > 0) Destroy(transform.GetChild(0).gameObject);
+                // PopulateList();
+            }
+        }
+
+        progressAction -= Time.deltaTime;
+    }
 
     public void PopulateList()
     {
