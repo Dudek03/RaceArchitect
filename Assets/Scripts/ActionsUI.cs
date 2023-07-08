d6c34a768c53ca6b0a5a9f9bb93368b95f13bf44
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-07-08 11:09:15 +0200

    somethings

--- Assets/Scripts/ActionsUI.cs
+++ Assets/Scripts/ActionsUI.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionsUI : MonoBehaviour
 {
@@ -16,6 +18,7 @@ public class ActionsUI : MonoBehaviour
     public List<Action> prefabs;
     public float timeAction = 3;
     private float progressAction = 0;
+    public Slider actionTimeSlider;
 
     private void Start()
     {
@@ -25,37 +28,45 @@ public class ActionsUI : MonoBehaviour
     private void Update()
     {
         if (GameManager.Instance.gameState != GameState.RUN) return;
-        print("sdasa");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MakeAction();
+        }
+
         if (progressAction <= 0)
         {
-            progressAction = timeAction;
-            if (GameManager.Instance.actionList.Count != 0)
-            {
-                var action = GameManager.Instance.actionList.FirstOrDefault();
-                switch (action)
-                {
-                    case ActionsTypes.UP:
-                        GameManager.Instance.car.ActionUp();
-                        break;
-                    case ActionsTypes.DOWN:
-                        GameManager.Instance.car.ActionDown();
-                        break;
-                    case ActionsTypes.LEFT:
-                        GameManager.Instance.car.ActionLeft();
-                        break;
-                    case ActionsTypes.RIGHT:
-                        GameManager.Instance.car.ActionRight();
-                        break;
-                }
-
-                GameManager.Instance.actionList.Remove(action);
-                //TODO
-                // while (transform.childCount > 0) Destroy(transform.GetChild(0).gameObject);
-                // PopulateList();
-            }
+            MakeAction();
         }
 
         progressAction -= Time.deltaTime;
+        actionTimeSlider.value = (timeAction - progressAction) / timeAction;
+    }
+
+    private void MakeAction()
+    {
+        progressAction = timeAction;
+        if (GameManager.Instance.actionList.Count != 0)
+        {
+            var action = GameManager.Instance.actionList.FirstOrDefault();
+            switch (action)
+            {
+                case ActionsTypes.UP:
+                    GameManager.Instance.car.ActionUp();
+                    break;
+                case ActionsTypes.DOWN:
+                    GameManager.Instance.car.ActionDown();
+                    break;
+                case ActionsTypes.LEFT:
+                    GameManager.Instance.car.ActionLeft();
+                    break;
+                case ActionsTypes.RIGHT:
+                    GameManager.Instance.car.ActionRight();
+                    break;
+            }
+
+            GameManager.Instance.actionList.Remove(action);
+            Destroy(transform.GetChild(0).gameObject);
+        }
     }
 
     public void PopulateList()
